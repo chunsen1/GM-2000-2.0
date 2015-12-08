14a38dd0182873cc552d29c06b4e2ba8c64276a6
chunsen1 <hunsen@web.de>
2015-12-08 18:23:34 +0100

    less debug prints for buttons

--- gm-2000/buttons.ino
+++ gm-2000/buttons.ino
@@ -8,24 +8,24 @@ uint8_t getButton() {
   if (keyValue > 790) return BUTTON_NONE;
   if (keyValue < 50) {
     //return BUTTON_UP;
-    DEBUG_PRINTLN(F("BUTTON_RIGHT"));
+    //DEBUG_PRINTLN(F("BUTTON_RIGHT"));
     return BUTTON_RIGHT;
   }
   if (keyValue < 195) {
     //return BUTTON_RIGHT;
-    DEBUG_PRINTLN(F("BUTTON_UP"));
+    //DEBUG_PRINTLN(F("BUTTON_UP"));
     return BUTTON_UP;
   }
   if (keyValue < 380) {
-    DEBUG_PRINTLN(F("BUTTON_DOWN"));
+    //DEBUG_PRINTLN(F("BUTTON_DOWN"));
     return BUTTON_DOWN;
   }
   if (keyValue < 555) {
-    DEBUG_PRINTLN(F("BUTTON_LEFT"));
+    //DEBUG_PRINTLN(F("BUTTON_LEFT"));
     return BUTTON_LEFT;
   }
   if (keyValue < 790) {
-    DEBUG_PRINTLN(F("BUTTON_SELECT"));
+    //DEBUG_PRINTLN(F("BUTTON_SELECT"));
     return BUTTON_SELECT;
   }
 
